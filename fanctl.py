b17c3543dbb43621a4881dce56130db9cc48c5d2
Dmitri Afanasjev <adimas@gmail.com>
2020-01-10 23:34:42 +0200

    Inverse PWM logic & Thermal zone selection from config.json

--- fanctl.py
+++ fanctl.py
@@ -11,27 +11,45 @@ try:
 	FAN_MAX_TEMP=config["FAN_MAX_TEMP"]
 	UPDATE_INTERVAL=config["UPDATE_INTERVAL"]
 	MAX_PERF=config["MAX_PERF"]
+	INV_PWM=config["INV_PWM"]
+	THERMAL_ZONE=config["THERMAL_ZONE"]
 except:
 	print("error loading /etc/automagic-fan/config.json.\nPlease check your config file.\nProceeding with default settings.")
 	FAN_OFF_TEMP=20
 	FAN_MAX_TEMP=50
 	UPDATE_INTERVAL=2
 	MAX_PERF=0
+	INVERSE_PWM=0
+	THERMAL_ZONE=0
+
+def read_type(thermal_zone = 0):
+	with open(f"/sys/devices/virtual/thermal/thermal_zone{thermal_zone}/type","r") as file:
+		result=file.read()
+	return result
+
+print("""Binding to thermal zone: {thermal_zone} Type: {zone_type}""".format(thermal_zone = THERMAL_ZONE, zone_type = read_type(THERMAL_ZONE)))
 
 if MAX_PERF>0:
-	print("Maximizing clock speeds with jetson_clocks")
+	print("Maximizing clock speeds with jetson_clocks ...")
 	sp.call("jetson_clocks")
 
+if INVERSE_PWM>0:
+	print("Fan PWM logic MODE: [inversed]")
+else:
+	print("Fan PWM logic MODE: [normal]")	
 
-def read_temp():
-	with open("/sys/devices/virtual/thermal/thermal_zone0/temp","r") as file:
-		temp_raw=file.read()
-	temp=int(temp_raw)/1000
-	return temp
+def read_temp(thermal_zone = 0):
+	with open(f"/sys/devices/virtual/thermal/thermal_zone{thermal_zone}/temp","r") as file:
+		result=file.read()
+	result=int(result)/1000
+	return result
 
-def fan_curve(temp):
+def fan_curve(temp, inversed = False):
 	spd=255*(temp-FAN_OFF_TEMP)/(FAN_MAX_TEMP-FAN_OFF_TEMP)
-	return int(min(max(0,spd),255))
+	if inversed:
+		return 255-int(min(max(0,spd),255))
+	else:	
+		return int(min(max(0,spd),255))
 
 def set_speed(spd):
 	with open("/sys/devices/pwm-fan/target_pwm","w") as file:
@@ -39,8 +57,8 @@ def set_speed(spd):
 
 print("Setup complete.\nRunning normally.")
 while True:
-	temp=read_temp()
-	spd=fan_curve(temp)
+	temp=read_temp(THERMAL_ZONE)
+	spd=fan_curve(temp, (INVERSE_PWM > 0))
 	out=set_speed(spd)
 	time.sleep(UPDATE_INTERVAL)
 
